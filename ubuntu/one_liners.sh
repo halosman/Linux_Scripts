05b7122f9b71985f683c903d764fed22815a4097
scottsquatch <sdose27@gmail.com>
2020-07-10 23:21:55 -0700

    Update one_liners.sh

--- ubuntu/one_liners.sh
+++ ubuntu/one_liners.sh
@@ -5,4 +5,4 @@
 # In my case the books were strucutred like /mnt/calibre/{filetype}/{filename}.{filetype} but they needed to be structured 
 # like /mnt/calibre/calibre-library/{filename}/{filename}.{filetype} (don't ask about destination path :(). 
 # This one liner accomplishes this; note that xargs is needed because file names have special characters (like space and parenthesis).
-for i in /mnt/calibre/*/*.*; do filename=$(basename -- "$i"); dirname=/mnt/calibre/calibre-library/${filename%.*}; mkdir -p $dirname && xargs -0 cp -n $i $dirname/$filename; done
+for i in /mnt/calibre/*/*.*; do filename=$(basename -- "$i"); dirname=/mnt/calibre/calibre-library/${filename%.*}; mkdir -p -- "$dirname" && cp -n -- "$i" "$dirname"/"$filename"; done
